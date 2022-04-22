2010445446610645519239920906a713d65b01a2
Divy Parekh <divyparekh1810@gmail.com>
2022-04-22 22:06:50 +0530

    Functionality added

---
+++ src/Components/BuyPage.js
@@ -0,0 +1,14 @@
+import React,{useState, useEffect} from "react";
+// https://myjson.dit.upm.es/api/bins/bo9h
+import { Axios } from "axios";
+import {random, commerce} from "faker"; 
+import {Container,Col,Row} from "reactstrap";
+
+const apiKey = "INSERT_YOUR_KEY_HERE";
+const url = "https://api.pexels.com/v1/search?query=laptop&per_page=6&page=1";
+
+const BuyPage = ({addInCart}) => {
+
+}
+
+export default BuyPage;
